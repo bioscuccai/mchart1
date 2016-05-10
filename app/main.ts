d8879e3b68d6449960965039ee5fa884b20251ff
goracz.akos <akos.zoltan.goracz@gmail.com>
2016-05-10 13:47:19 +0200

    takaritas

--- app/main.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import AppComp from './AppComp';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import ReadingService from './ReadingService';
-
-bootstrap(AppComp, [HTTP_PROVIDERS, ReadingService]);
-
